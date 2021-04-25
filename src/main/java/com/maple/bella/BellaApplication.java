dfee0c0c8efe9126f0ba9fb96a7c3f8a0326cac6
maple <gorpago@naver.com>
2021-04-25 17:02:51 +0900

    init

---
+++ src/main/java/com/maple/bella/BellaApplication.java
@@ -0,0 +1,13 @@
+package com.maple.bella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BellaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maple/bella/BellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.bella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
